ef659db2a1471ece79a9052fc3053a733edc3578
Swaraj Mate <mateswaraj07@gmail.com>
2025-07-03 18:39:31 +0530

    taks

--- ShadowFox/Task1/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const section = document.querySelector(this.getAttribute('href'));
-    section.scrollIntoView({ behavior: 'smooth' });
-  });
-});
